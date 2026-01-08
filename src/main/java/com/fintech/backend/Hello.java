17731b23a7de4c5bcac6498a2f25ec090f275fb1
Darshith7 <saidarshith@gmail.com>
2026-01-08 15:47:38 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/fintech/backend/Hello.java
@@ -0,0 +1,10 @@
+package com.fintech.backend;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class Hello{
+                @GetMapping("/")
+public String index() { 
+return "Greetings from MIT-FIS!";
+    }
+}
